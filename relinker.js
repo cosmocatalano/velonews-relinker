64eb15ef8f55914ed09b46717baeec9e2ab967a7
Cosmo Catalano <cosmo.catalano@gmail.com>
2021-07-08 10:25:31 -0400

    Update relinker.js
    deminifying relinker.js

--- relinker.js
+++ relinker.js
@@ -1,1 +1,54 @@
-function clearEnd(e,t,n=1){let o=!1;return o=e[e.length-n]===t?clearEnd(e,t,n+1):e.substr(0,e.length-(n-1)),o}const vnBase="https://velonews.com?p=";let postId="";document.addEventListener("DOMContentLoaded",(function(e){let t=document.querySelectorAll("a");for(link of t){let e=new URL(link.href);if("velonews.competitor.com"===e.hostname||"velonews.com"===e.hostname){if(console.log(e.pathname),e.search)postId=e.search;else{let t=clearEnd(e.pathname,"/");postId=-1!==t.indexOf("_")?t.split("_")[1]:t.split("/")[t.split("/").length-1]}link.dataset.oldUrl=e,link.href=vnBase+postId,console.log(vnBase+postId)}}}));
+//because JS is bad at trimming
+function clearEnd(target, badChar, pos = 1) {
+	let cleared = false;
+	if ( target[target.length - pos] === badChar) {
+		cleared = clearEnd(target, badChar, pos + 1);
+	} else {
+		cleared = target.substr(0, (target.length - (pos - 1) ) )
+	}
+	return cleared;
+}
+
+//a usable URL base
+const vnBase = "https://velonews.com?p="
+
+//a place to put the WP post ID
+let postId = "";
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+	//get all the links
+	let pageLinks = document.querySelectorAll("a");
+
+	for (link of pageLinks) {
+
+		//make each href attribute a URL
+		let linkUrl = new URL(link.href);
+
+		//is it velonews?
+		if (linkUrl.hostname === "velonews.competitor.com" || linkUrl.hostname === "velonews.com") {
+			console.log(linkUrl.pathname);
+
+			//if there's no direct query id
+			if (!linkUrl.search) {
+
+				//remove the trailing slash
+				let noSlash = clearEnd(linkUrl.pathname, '/');
+
+				//underscore always sets off the WP post ID
+				if ( noSlash.indexOf('_') !== -1 ) {
+					postId = noSlash.split('_')[1];
+				} else {
+					//otherwise we can be fairly certain it's the last section of the pathname
+					postId = noSlash.split("/")[noSlash.split("/").length - 1];
+				}
+			//direct query ID always works
+			} else {
+				postId = linkUrl.search;
+			}
+			link.dataset.oldUrl = linkUrl;
+			link.href = vnBase + postId;
+			console.log(vnBase + postId)
+		}
+	}
+});
